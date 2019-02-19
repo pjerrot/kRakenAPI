ecf890e8ed2ea338f70830bfb9d469947dcd0b20
pjerrot <john.westberg@gmail.com>
2019-02-19 14:47:25 +0100

    New version of kraken_get_ticker function

--- kraken_api.r
+++ kraken_api.r
@@ -212,6 +212,17 @@ kraken_get_ticker <- function(pair) {
   return(query_result)
 }
 
+
+kraken_get_ticker <- function(pair) {
+  url <- "https://api.kraken.com/0/public/Ticker"
+  pair <- paste(pair,collapse=",")
+  req <- GET(url, query = list(pair = pair))
+  stop_for_status(req)
+  json <- content(req, "text")
+  query_result <- fromJSON(json)
+  return(query_result)
+}
+
 #Output
 #    a = ask array(<price>, <whole lot volume>, <lot volume>),
 #    b = bid array(<price>, <whole lot volume>, <lot volume>),
